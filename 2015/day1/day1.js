922691c839e28a4cf8a0fcc327e7917cf5579ce3
Andrew Gray <andrew.aj.gray@gmail.com>
2024-03-09 12:52:18 +1030

    completed 2015 day 1 part 2

--- 2015/day1/day1.js
+++ 2015/day1/day1.js
@@ -6,7 +6,19 @@ function part1(input) {
     .reduce((total, current) => (current === "(" ? total + 1 : total - 1), 0);
 }
 
+function part2(input) {
+  let output = 0;
+  for (c in input) {
+    if (input[c] == "(") output++;
+    else output--;
+    if (output < 0) {
+      return ++c;
+    }
+  }
+}
+
 const filename = process.argv[2];
 readFromFile(filename, (fileData) => {
   console.log("Part 1:", part1(fileData));
+  console.log("Part 2:", part2(fileData));
 });
